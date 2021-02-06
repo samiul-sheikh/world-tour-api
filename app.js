eff392bdb4b08ae5da8e637a53b617c17d42bad6
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-02-07 05:25:53 +0600

    creat a lot of country with style

--- app.js
+++ app.js
@@ -9,12 +9,19 @@ const displayCountries = countries => {
         const country = countries[i];
         const countryDiv = document.createElement('div');
 
-        const h3 = document.createElement('h3');
-        h3.innerText = country.name;
-        const p = document.createElement('p');
-        p.innerText = country.capital;
-        countryDiv.appendChild(h3);
-        countryDiv.appendChild(p);
+        // const h3 = document.createElement('h3');
+        // h3.innerText = country.name;
+        // const p = document.createElement('p');
+        // p.innerText = country.capital;
+        // countryDiv.appendChild(h3);
+        // countryDiv.appendChild(p);
+
+        countryDiv.className = 'country';
+        const countryInfo = `
+            <h3 class="country-name">${country.name}</h3>
+            <p class="capital">${country.capital}</p>
+        `;
+        countryDiv.innerHTML = countryInfo;
 
         countriesDiv.appendChild(countryDiv);
     }
