5905407d540e3ae3954c2f6077bd1f1db3940137
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-02-07 04:31:13 +0600

    display all country names & capital in UI as div

--- app.js
+++ app.js
@@ -3,14 +3,19 @@ fetch('https://restcountries.eu/rest/v2/all')
     .then(data => displayCountries(data));
 
 const displayCountries = countries => {
-    // console.log(countries);
-    // display all country names in UI
-    const ul = document.getElementById('countries');
+    // display all country names in UI as div
+    const countriesDiv = document.getElementById('countries');
     for (let i = 0; i < countries.length; i++) {
         const country = countries[i];
-        // console.log(country.name);
-        const li = document.createElement('li');
-        li.innerText = country.name;
-        ul.appendChild(li);
+        const countryDiv = document.createElement('div');
+
+        const h3 = document.createElement('h3');
+        h3.innerText = country.name;
+        const p = document.createElement('p');
+        p.innerText = country.capital;
+        countryDiv.appendChild(h3);
+        countryDiv.appendChild(p);
+
+        countriesDiv.appendChild(countryDiv);
     }
 }
