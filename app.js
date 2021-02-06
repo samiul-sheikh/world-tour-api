1c220960a0106e4139cfb24eca1c15d96d02054f
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-02-07 05:52:26 +0600

    using foreach loop alter for loop

--- app.js
+++ app.js
@@ -3,11 +3,30 @@ fetch('https://restcountries.eu/rest/v2/all')
     .then(data => displayCountries(data));
 
 const displayCountries = countries => {
+    const countriesDiv = document.getElementById('countries');
+    countries.forEach(country => {
+        const countryDiv = document.createElement('div');
+
+        countryDiv.className = 'country';
+        const countryInfo = `
+            <h3 class="country-name">${country.name}</h3>
+            <p class="capital">${country.capital}</p>
+        `;
+        countryDiv.innerHTML = countryInfo;
+
+        countriesDiv.appendChild(countryDiv);
+    });
+}
+
+/*
+// using for loop
+const displayCountries = countries => {
     // display all country names in UI as div
     const countriesDiv = document.getElementById('countries');
     for (let i = 0; i < countries.length; i++) {
         const country = countries[i];
         const countryDiv = document.createElement('div');
+
 
         // const h3 = document.createElement('h3');
         // h3.innerText = country.name;
@@ -15,6 +34,7 @@ const displayCountries = countries => {
         // p.innerText = country.capital;
         // countryDiv.appendChild(h3);
         // countryDiv.appendChild(p);
+
 
         countryDiv.className = 'country';
         const countryInfo = `
@@ -26,3 +46,4 @@ const displayCountries = countries => {
         countriesDiv.appendChild(countryDiv);
     }
 }
+*/
