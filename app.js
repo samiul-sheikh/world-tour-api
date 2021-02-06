586e000eb6cc4b637a54afcf9c88d59907740b50
samiul-sheikh <sheikh.samiul1971@gmail.com>
2021-02-07 04:12:07 +0600

    display all country names in UI as li

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
 fetch('https://restcountries.eu/rest/v2/all')
-.then(Response => Response.json())
-.then(data => console.log(data));
+    .then(Response => Response.json())
+    .then(data => displayCountries(data));
+
+const displayCountries = countries => {
+    // console.log(countries);
+    // display all country names in UI
+    const ul = document.getElementById('countries');
+    for (let i = 0; i < countries.length; i++) {
+        const country = countries[i];
+        // console.log(country.name);
+        const li = document.createElement('li');
+        li.innerText = country.name;
+        ul.appendChild(li);
+    }
+}
